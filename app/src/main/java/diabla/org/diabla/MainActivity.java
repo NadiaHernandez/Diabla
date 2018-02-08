f63641ce627c9c71fb820504f122ef03495eff68
Nadia <nadiafrias.h@gmail.com>
2018-02-08 17:22:08 -0600

    malo

--- app/src/main/java/diabla/org/diabla/MainActivity.java
+++ app/src/main/java/diabla/org/diabla/MainActivity.java
@@ -2,6 +2,7 @@ package diabla.org.diabla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //MalditoVirustt
+
     }
 }
